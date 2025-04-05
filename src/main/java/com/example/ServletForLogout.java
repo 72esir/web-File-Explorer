8e4b8c6900ebe80b3df2ff55fd922400b8556652
Aleksey Salamatin <asalamatin640@gmail.com>
2025-04-05 13:16:05 +0500

    channge logout servlet name

--- src/main/java/com/example/ServletForLogout.java
+++ src/main/java/com/example/ServletForLogout.java
@@ -1,15 +1,13 @@
 package com.example;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/logout")
-public class LogoutServlet extends HttpServlet {
+public class ServletForLogout extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.getSession().invalidate();
         resp.sendRedirect("login");
